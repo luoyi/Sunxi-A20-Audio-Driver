2a4179df112bf191d73bd76ee22d4075f4c45bb1
luoyi <luoyi.ly@gmail.com>
2016-05-23 11:01:10 +0800

    Revert "add A20 i2s support"
    This reverts commit 6fa0f14bf949bcaea1951ad14a0d924b9c289859.
    
    add init.sh is wrong

--- DeviceTree/init.sh
+++ DeviceTree/init.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 wget -O sun7i-a20.dtsi.orig https://raw.githubusercontent.com/torvalds/linux/master/arch/arm/boot/dts/sun7i-a20.dtsi
-wget https://raw.githubusercontent.com/torvalds/linux/master/arch/arm/boot/dts/skeleton.dtsi
-wget wget https://raw.githubusercontent.com/torvalds/linux/master/arch/arm/boot/dts/sunxi-common-regulators.dtsi
