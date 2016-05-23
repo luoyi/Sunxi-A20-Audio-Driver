65f911cce91ca60e71cb7d1bfe7374a489ac6754
luoyi <luoyi.ly@gmail.com>
2016-05-23 11:08:15 +0800

    add some more files to get in init.sh

--- DeviceTree/init.sh
+++ DeviceTree/init.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 wget -O sun7i-a20.dtsi.orig https://raw.githubusercontent.com/torvalds/linux/master/arch/arm/boot/dts/sun7i-a20.dtsi
+wget https://raw.githubusercontent.com/torvalds/linux/master/arch/arm/boot/dts/skeleton.dtsi
+wget https://raw.githubusercontent.com/torvalds/linux/master/arch/arm/boot/dts/sunxi-common-regulators.dtsi
